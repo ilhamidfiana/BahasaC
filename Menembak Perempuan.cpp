e0771a80f0bddd64b5051a9a512ab93ba1b71dc4
ilhamidfiana <88029063+ilhamidfiana@users.noreply.github.com>
2021-08-02 18:15:04 +0700

    Add files via upload

---
+++ Menembak Perempuan.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <stdlib.h>
+#include <Windows.h>
+#include <stdio.h>
+#include <iomanip>
+#include <conio.h>
+#include <unistd.h>
+#include <cstdlib>
+using namespace std;
+int main(){
+	int choose;
+		awal :
+		system ("cls");
+		cout<<"Apakah anda cinta? pilih angka genap dan ganjil"<<endl;
+        cout<<endl;
+        cout<<"1. 1(True)"<<endl;
+        cout<<"2. 0(False)"<<endl;
+        cout<<endl;
+        cout<<"Masukkan Pilihan Anda = ";cin>>choose;
+        if(choose == 1){
+        	cout<<"Pilihan anda sangat tepat :D"<<endl;
+        	sleep(2);
+        	cout<<"I Love Youuuuu :D........."<<endl;
+		}
+		else{
+			cout<<"Gak bisa harus pilih 1 / True :V (3 detik)"<<endl;
+			sleep(3);
+			goto awal;
+			
+		}
+    return 0;
+}
---
+++ minta maaf.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i = 1;
+	for(i=0;i<=5;i++){
+		cout<<"Aku Minta Maaf Ya :("<<endl;
+	}
+}
