ee5f4b59ff0f506ae131cd64c62bbca4cb36a07f
IlhamArham <88029063+ilhamidfiana@users.noreply.github.com>
2021-08-26 13:06:11 +0700

    Add files via upload

---
+++ Percobaanku.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <stdlib.h>
+#include <Windows.h>
+#include <stdio.h>
+#include <iomanip>
+#include <conio.h>
+#include <unistd.h>
+#include <cstdlib>
+using namespace std;
+int main(){
+	int choose;
+	string nama;
+		awal :
+		system ("cls");
+		cout<<"Masukkan Nama Anda : ";cin>>nama;cout<<endl;
+		sleep(2);
+		cout<<"Hi "<<nama;cout<<" Gimana kabarmu? ";sleep(2);cout<<" Berharap baik ya :D";sleep(2);cout<<endl;
+		cout<<" Aku C++ Pintar, denger-denger kamu sebentar lagi akan wisuda ya? "<<nama<<endl;sleep(3);
+		cout<<" selamat ya ada program yang ingin bilang ke kamu "<<nama<<endl;sleep(2);
+		cout<<" 3 ";sleep(1);cout<<" 2 ";sleep(1);cout<<" 1 ";sleep(1);cout<<endl;
+		system ("love.exe");
+    	system ("pause");
+    return 0;
+}
---
+++ love.cpp
@@ -0,0 +1,189 @@
+#include "iostream"
+#include <cstdlib>
+#include "conio.h"
+
+using namespace std;
+int main(void)
+{
+ int i;
+ int j;
+ int k;
+ j=50;
+ k=70;
+ for(i=1;i<=j;i++)
+ {
+     system("cls");
+
+  cout<<"Jangan Di Close Dulu Sebelum abis\n";
+ }
+ for(i=1;i<=j;i++)
+ {
+     system("cls");
+
+  cout<<"Aktivin koneksi internet terlebih dahulu\n";
+ }
+ for(i=1;i<=j;i++)
+ {
+     system("cls");
+
+  cout<<"Sebelum kamu lanjutin aplikasi ini lu jangan marah ya\n";
+ }
+ for(i=1;i<=k;i++)
+ {
+     system("cls");
+
+  cout<<"Sebenernya\n";
+ }
+ for(i=1;i<=k;i++)
+ {
+     system("cls");
+
+  cout<<"Aku\n";
+ }
+ for(i=1;i<=k;i++)
+ {
+     system("cls");
+
+  cout<<"Suka\n";
+ }
+ for(i=1;i<=k;i++)
+ {
+     system("cls");
+
+  cout<<"Sama\n";
+ }
+ for(i=1;i<=k;i++)
+ {
+     system("cls");
+
+  cout<<"Kamu\n";
+ }
+ for(i=1;i<=k;i++)
+ {
+     system("cls");
+
+  cout<<"Aku harap kamu bisa nerima gw apa adanya\n";
+ }
+ for(i=1;i<=k;i++)
+ {
+     system("cls");
+
+  cout<<"Kalau kamu gk mau nerima aku jg gpp yang penting aku udah ungkapin semuanya\n";
+ }
+ for(i=1;i<=j;i++)
+ {
+     system("cls");
+
+  cout<<"Ya mungkin itu aja.\n";
+ }
+ for(i=1;i<=j;i++)
+ {
+     system("cls");
+  cout<<"Aku Cuman Bisa ngasih kamu gambar ini\n";
+ }
+ for(i=1;i<=22;i++)
+ {
+  for(j=1;j<=60;j++)
+  {
+   if(i==1)
+    if(((j>=14)&&(j<=23))||((j>=39)&&(j<=48)))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==2)
+    if(((j>=11)&&(j<=26))||((j>=36)&&(j<=51)))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==3)
+    if(((j>=9)&&(j<=28))||((j>=34)&&(j<=53)))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==4)
+    if(((j>=7)&&(j<=30))||((j>=32)&&(j<=55)))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if((i>=5)&&(i<=8))
+    if((j>=5)&&(j<=57))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==9)
+    if((j>=7)&&(j<=55))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==10)
+    if((j>=9)&&(j<=53))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==11)
+    if((j>=11)&&(j<=51))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==12)
+    if((j>=13)&&(j<=49))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==13)
+    if((j>=15)&&(j<=47))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==14)
+    if((j>=17)&&(j<=45))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==15)
+    if((j>=19)&&(j<=43))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==16)
+    if((j>=21)&&(j<=41))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==17)
+    if((j>=23)&&(j<=39))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==18)
+    if((j>=25)&&(j<=37))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==19)
+    if((j>=27)&&(j<=35))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==20)
+    if((j>=29)&&(j<=33))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==21)
+    if((j>=30)&&(j<=32))
+     cout<<"*";
+    else
+     cout<<" ";
+   else if(i==22)
+    if(j==31)
+     cout<<"*";
+   else
+    cout<<" ";
+  }
+  cout<<"\n";
+  }
+  cout<<"\n";
+  cout<<"\n               Create By ARHAMHAM'z' Anda For U \n";
+  getch();
+  }
